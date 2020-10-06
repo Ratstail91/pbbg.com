c2664a9ea9a43292410e66f77e5255ef5263f9df
FoohonPie <674157+foohonpie@users.noreply.github.com>
2020-10-06 15:50:12 -0500

    lint

--- database/migrations/2016_06_01_000005_create_oauth_personal_access_clients_table.php
+++ database/migrations/2016_06_01_000005_create_oauth_personal_access_clients_table.php
@@ -30,11 +30,13 @@ class CreateOauthPersonalAccessClientsTable extends Migration
      */
     public function up()
     {
-        $this->schema->create('oauth_personal_access_clients', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->uuid('client_id');
-            $table->timestamps();
-        });
+        $this->schema->create(
+            'oauth_personal_access_clients', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->uuid('client_id');
+                $table->timestamps();
+            }
+        );
     }
 
     /**
