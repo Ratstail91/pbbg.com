7fe5c9d4d686976f58ae6f07a42274f159487de2
FoohonPie <674157+foohonpie@users.noreply.github.com>
2020-10-11 19:12:10 -0500

    cleaning up registration instances in tests

--- tests/Feature/Users/GetAuthenticatedUserTest.php
+++ tests/Feature/Users/GetAuthenticatedUserTest.php
@@ -15,19 +15,9 @@ class GetAuthenticatedUserTest extends TestCase
      */
     public function testAnAuthenticatedUserCanAccessAuthenticatedUserData()
     {
-        $random_uuid = uniqid();
+        $token = $this->registerUser();
         $response = $this->withHeaders([
             'Accept' => 'application/json',
-        ])->post('/register', [
-            'name' => 'User_' . $random_uuid,
-            'email' => 'foo_' . $random_uuid . '@bar.baz',
-            'password' => 'foobarbaz'
-        ]);
-        $response_json = json_decode($response->content());
-        $token = $response_json->token;
-        $this->assertNotEquals('', $token); # improve this to check that token is valid JWT
-
-        $response = $this->withHeaders([
             'Authorization' => "Bearer $token"
         ])->get('/user');
         $response->assertStatus(200);
@@ -40,22 +30,8 @@ class GetAuthenticatedUserTest extends TestCase
      */
     public function testAnUnauthenticatedUserCanNotAccessAuthenticatedUserData()
     {
-        $random_uuid = uniqid();
         $response = $this->withHeaders([
             'Accept' => 'application/json',
-        ])->post('/register', [
-            'name' => 'User_' . $random_uuid,
-            'email' => 'foo_' . $random_uuid . '@bar.baz',
-            'password' => 'foobarbaz'
-        ]);
-        $response_json = json_decode($response->content());
-        $token = $response_json->token;
-        $this->assertNotEquals('', $token); # improve this to check that token is valid JWT
-
-        $response = $this->get('/user');
-        $response->assertStatus(401);
-
-        $response = $this->withHeaders([
             'Authorization' => "Bearer InvalidToken"
         ])->get('/user');
         $response->assertStatus(401);
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,19 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function registerUser($uuid = null) {
+        $uuid = $uuid ?: uniqid();
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+        ])->post('/register', [
+            'name' => 'User_' . $uuid,
+            'email' => 'foo_' . $uuid . '@bar.baz',
+            'password' => 'foobarbaz'
+        ]);
+        $response_json = json_decode($response->content());
+        $token = $response_json->token;
+        $this->assertNotEquals('', $token); # improve this to check that token is valid JWT
+        return $token;
+    }
 }
