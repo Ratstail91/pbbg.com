52e2bc477af31c85c915f4b97d752dadf8c8f3c9
Zac Olden <zacolden@gmail.com>
2020-10-28 22:42:58 +1100

    Require authentication to delete all users

--- tests/Feature/Users/DeleteUsersTest.php
+++ tests/Feature/Users/DeleteUsersTest.php
@@ -5,12 +5,34 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
 use Tests\TestCase;
 
 class DeleteUsersTest extends TestCase
 {
     /**
-     * Get list of users
+     * Setup the test environment.
+     * Deletes existing roles and permissions before running a test.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        DB::statement('TRUNCATE TABLE model_has_roles;');
+
+        // turn off FK constraint to allow truncate
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        Role::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
+
+        Role::create(['name' => 'admin']);
+    }
+
+    /**
+     * Tests that the authorized user can delete all users.
      *
      * @return void
      */
@@ -19,12 +41,38 @@ class DeleteUsersTest extends TestCase
         $this->registerUser();
 
         $count = User::count();
-        $this->assertGreaterThan(0,$count);
+        $this->assertGreaterThan(0, $count);
+
+        $user = User::create([
+            'name' => 'user_'.uniqid(),
+            'email' => 'test_'.uniqid().'@test.test',
+            'password' => encrypt('password'),
+        ]);
+
+        $this->actingAs($user);
 
         $response = $this->withHeaders(['Accept' => 'application/json'])->delete('/users');
+
         $this->assertResponse($response, 200);
 
         $count = User::count();
-        $this->assertEquals(0,$count);
+        $this->assertEquals(0, $count);
+    }
+
+    /**
+     * Tests that an unauthenticated user cannot delete all users.
+     *
+     * @return void
+     */
+    public function testDeleteUsersWithoutAuthentication()
+    {
+        $this->registerUser();
+
+        $count = User::count();
+        $this->assertGreaterThan(0, $count);
+
+        $response = $this->withHeaders(['Accept' => 'application/json'])->delete('/users');
+
+        $this->assertResponse($response, 401);
     }
 }
